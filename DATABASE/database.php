5158489927a6e0012731c138516543ad7ce1dcfc
Matthew <realazurenova@gmail.com>
2026-03-10 13:12:55 +0100

    Added database

---
+++ DATABASE/database.php
@@ -0,0 +1,32 @@
+<?php
+    $dsn = "mysql:host=127.0.0.1;dbname=zoodatabase;charset=utf8mb4";
+    $username = "zoomanagement";
+    $password = "4q=2L*7(/^tGcxC?";
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //echo "Success";
+    }
+    catch (PDOException $e){
+        echo "Unable to connect DB: ". $e->getMessage();
+        exit();
+    }
+
+    function get($table, $id): mixed{
+        global $db;
+        $sql = "SELECT * FROM $table WHERE id = :id";
+        $stmt = $db->prepare($sql);
+        $stmt->execute(['id'=>$id]);
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    function getALL($table): array{
+        global $db;
+        $sql = "SELECT * FROM $table";
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetchALL(PDO::FETCH_ASSOC);
+    }
+?>
